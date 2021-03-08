5a1f4b089b0327ab2242d3e64dc7dcd2dc1e0661
student00010671 <student00010671@gmail.com>
2021-03-08 02:30:15 -0500

    add all notes page

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get('/create_note', (req, res) => {
     res.render('create_note')
 })
 
+app.get('/notes', (req, res) => {
+    const notes = ['Some awesome note 1', 'Some awesome note 2', 'Some awesome note 3']
+    res.render('all_notes',  { notes: notes })
+})
+
 app.listen(8000, err => {
     if (err) throw err
     console.log('App is running ...')
